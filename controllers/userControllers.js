0613270c16673089954d7e658b98c5139acfc928
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-01-29 11:45:45 -0500

    removed .populate(thought) from user model. Removed duplicated resopnses in add friend route

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,7 +3,6 @@ import { User, Thought } from "../models/index.js";
 // * GET router.route("/").get(getAllUsers);
 export function getAllUsers(req, res) {
 	User.find()
-		.populate("Thought")
 		.then((users) => res.json(users))
 		.catch((err) => {
 			console.error(err);
@@ -14,7 +13,6 @@ export function getAllUsers(req, res) {
 // * GET router.route("/:userId").get(getOneUser);
 export function getOneUser(req, res) {
 	User.findOne({ _id: req.params.userId })
-		.populate("Thought")
 		.then((user) => res.json(user))
 		.catch((err) => {
 			console.error(err);
@@ -81,50 +79,84 @@ export function addFriend(req, res) {
 	// * Friends field in user model is array of user schemas. A user object, not an ID, must be added to the array.
 
 	const user = User.findOne({ _id: req.params.userId })
-		.populate("Thought")
-		.then((user) => res.json(user))
+		.then((user) => {
+			if (!user) {
+				// * If no user exists, throw an error to prevent mongoose from trying to reference a non-existent user
+				throw new ReferenceError(
+					`No user with this id: ${req.params.userId}`
+				);
+			}
+		})
 		.catch((err) => {
 			console.error(err);
-			return res.status(500).json(err);
+			return err;
 		});
 
 	const friend = User.findOne({ _id: req.params.friendId })
-		.populate("Thought")
-		.then((user) => res.json(user))
+		.then((friend) => {
+			if (!friend) {
+				// * If no user exists, throw an error to prevent mongoose from trying to reference a non-existent user
+				throw new ReferenceError(
+					`No user with this id: ${req.req.params.friendId}`
+				);
+			}
+		})
 		.catch((err) => {
 			console.error(err);
-			return res.status(500).json(err);
+			return err;
 		});
 
-	User.findByIdAndUpdate(
+	if (user instanceof Error || friend instanceof Error) {
+		return res
+			.status(500)
+			.json({ userResponse: user, friendResponse: friend });
+	}
+
+	// TODO: Need to verify if user is already added to friends list before attempting to add
+
+	const updatedUser = User.findByIdAndUpdate(
 		req.params.userId,
 		{ $push: { friends: friend } },
 		{ new: true }
 	)
-		.then((user) =>
-			!user
-				? res.status(404).json({ message: "No user with this id!" })
-				: res.json(user)
-		)
+		.then((user) => {
+			if (!user) {
+				// * If no user exists, throw an error to prevent mongoose from trying to reference a non-existent user
+				throw new ReferenceError(
+					`No user with this id: ${req.params.userId}`
+				);
+			}
+		})
 		.catch((err) => {
 			console.error(err);
-			return res.status(500).json(err);
+			return err;
 		});
 
-	User.findByIdAndUpdate(
+	const updatedFriend = User.findByIdAndUpdate(
 		req.params.friendId,
 		{ $push: { friends: user } },
 		{ new: true }
 	)
-		.then((user) =>
-			!user
-				? res.status(404).json({ message: "No user with this id!" })
-				: res.json(user)
-		)
+		.then((friend) => {
+			if (!friend) {
+				// * If no user exists, throw an error to prevent mongoose from trying to reference a non-existent user
+				throw new ReferenceError(
+					`No user with this id: ${req.req.params.friendId}`
+				);
+			}
+		})
 		.catch((err) => {
 			console.error(err);
-			return res.status(500).json(err);
+			return err;
 		});
+
+	if (updatedUser instanceof Error || updatedFriend instanceof Error) {
+		return res
+			.status(500)
+			.json({ userResponse: updatedUser, friendResponse: updatedFriend });
+	} else {
+		return res.json({ user: updatedUser, friend: updatedFriend });
+	}
 }
 
 // * DELETE (PUT) router.route("/:userId/friends/:friendId").delete(deleteFriend);
@@ -132,7 +164,6 @@ export function deleteFriend(req, res) {
 	// * Friends field in user model is array of user schemas. A user object, not an ID, must be removed from the array.
 
 	const user = User.findOne({ _id: req.params.userId })
-		.populate("Thought")
 		.then((user) => res.json(user))
 		.catch((err) => {
 			console.error(err);
@@ -140,7 +171,6 @@ export function deleteFriend(req, res) {
 		});
 
 	const friend = User.findOne({ _id: req.params.friendId })
-		.populate("Thought")
 		.then((user) => res.json(user))
 		.catch((err) => {
 			console.error(err);
